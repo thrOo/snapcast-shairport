8829301044527d1be9417ebde8b7d279f17fb8f7
thrOo <throo@protonmail.com>
2022-05-08 15:16:14 +0200

    removed unused scripts

--- getLatestSnapserver.sh
+++
@@ -1,1 +0,0 @@
-wget $(curl -s https://api.github.com/repos/badaix/snapcast/releases/latest | jq '.assets[] | select(.name | test("snapserver.*?amd64.deb"))' | jq '.browser_download_url' | sed 's/"//g') 
--- installSnapserver.sh
+++
@@ -1,1 +0,0 @@
-apt-get install -y ./$(curl -s https://api.github.com/repos/badaix/snapcast/releases/latest | jq '.assets[] | select(.name | test("snapserver.*?amd64.deb"))' | jq '.name' | sed 's/"//g')
