ec9dab6cb5e026f314445c91b9a9336b56c63462
thrOo <throo@protonmail.com>
2022-05-08 12:27:13 +0200

    split snapserver get script and install script for ubuntu based image

---
+++ getLatestSnapserver.sh
@@ -0,0 +1,1 @@
+wget $(curl -s https://api.github.com/repos/badaix/snapcast/releases/latest | jq '.assets[] | select(.name | test("snapserver.*?amd64.deb"))' | jq '.browser_download_url' | sed 's/"//g') 
--- installSnapserver.sh
+++ installSnapserver.sh
@@ -1,1 +1,1 @@
-wget $(curl -s https://api.github.com/repos/badaix/snapcast/releases/latest | jq '.assets[] | select(.name | test("snapserver.*?amd64.deb"))' | jq '.browser_download_url' | sed 's/"//g')
+apt-get install -y ./$(curl -s https://api.github.com/repos/badaix/snapcast/releases/latest | jq '.assets[] | select(.name | test("snapserver.*?amd64.deb"))' | jq '.name' | sed 's/"//g')
